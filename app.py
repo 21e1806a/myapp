aabfd2a86666143de87cb10d98a7f00e8e89e73c
21e1806a <119286019+21e1806a@users.noreply.github.com>
2022-12-05 11:25:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -16,29 +16,23 @@ left_column.write(food1)
 import pandas as pd
 st.table(pd.DataFrame({
     '食材':[1, 2, 3, 4],
-    '賞味期限': [food1, food2, food3, food4]}))
-
-
-
-left_column,right_column = st.columns(2)  
-
-
-food1 = st.date_input('1つ目の食材の賞味期限はいつですか？',)
-left_column.write(food1)   
+    '賞味期限': [food1, 2,3,4
+             ]}))
+  
     
 @st.cache(allow_output_mutation=True)
 def cache_DateFrame():
-    pd.DateFrame = []
-    return pd.DateFrame
+    DateFrame = []
+    return DateFrame
 DateFrame = cache_DateFrame()
 input = st.text_input('食材は何ですか？')
 if input:
-    pd.DateFrame.append(input)    
+    DateFrame.append(input)    
 
 if st.checkbox('delete1'):
-    delete = st.selectbox('食べたらリストから削除しましょう', options=pd.DateFrame)
+    delete = st.selectbox('食べたらリストから削除しましょう', options=DateFrame)
     if st.button('Delete1'):
-        pd.DateFrame.remove(delete)
+        DateFrame.remove(delete)
         st.success(f'Delete1 : {delete}')
         
-st.table(pd.DateFrame)
+st.table(DateFrame)
