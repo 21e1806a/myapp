fefd4c2c4b4643f1c195f4d6f1959ee969ffc663
21e1806a <119286019+21e1806a@users.noreply.github.com>
2022-12-05 11:37:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,29 +10,28 @@ import streamlit as st
 st.title("冷蔵庫の賞味期限")
 
 left_column,right_column = st.columns(2)
-food1 = st.date_input('1つ目の食材の賞味期限はいつですか？',)
-left_column.write(food1)
+food1 = st.text_input('食材は何ですか？')
+term1 = st.date_input(food1,'の食材の賞味期限はいつですか？',)
+left_column.write(term1)
+
+food2 = st.text_input('食材は何ですか？')
+term2 = st.date_input(food2,'の賞味期限はいつですか？',)
+left_column.write(term2)
+
+food3 = st.text_input('食材は何ですか？')
+term3 = st.date_input(food3,'の賞味期限はいつですか？',)
+left_column.write(term3)
+
+food3 = st.text_input('食材は何ですか？')
+term3 = st.date_input(food3,'の賞味期限はいつですか？',)
+left_column.write(term3)
+
+food4 = st.text_input('食材は何ですか？')
+term4 = st.date_input(food4,'の賞味期限はいつですか？',)
+left_column.write(term4)
 
 import pandas as pd
 st.table(pd.DataFrame({
-    '食材':[1, 2, 3, 4],
-    '賞味期限': [food1, 2,3,4
+    '食材':[food1, food2, food3, food4],
+    '賞味期限': [term1, term2, term3, term4
              ]}))
-  
-    
-@st.cache(allow_output_mutation=True)
-def cache_DateFrame():
-    DateFrame = []
-    return DateFrame
-DateFrame = cache_DateFrame()
-input = st.text_input('食材は何ですか？')
-if input:
-    DateFrame.append(input)    
-
-if st.checkbox('delete1'):
-    delete = st.selectbox('食べたらリストから削除しましょう', options=DateFrame)
-    if st.button('Delete1'):
-        DateFrame.remove(delete)
-        st.success(f'Delete1 : {delete}')
-        
-st.table(DateFrame)
