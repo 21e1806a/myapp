70149c3d2ec8ef3e5f5341167b4ce561c04ed5cc
21e1806a <119286019+21e1806a@users.noreply.github.com>
2022-12-05 11:15:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,12 @@ import streamlit as st
 
 st.title("冷蔵庫の賞味期限")
 
+left_column,right_column = st.columns(2)
+food1 = st.date_input('1つ目の食材の賞味期限はいつですか？',)
+left_column.write(food1)
 import pandas as pd
 st.table(pd.DataFrame({
-    '食材': [1, 2, 3, 4],
+    '食材': [food1, 2, 3, 4],
     '賞味期限': [10, 20, 30, 40]}))
 
 
