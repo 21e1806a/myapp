9dcf5724ce10183963dee064894288eb406da97f
21e1806a <119286019+21e1806a@users.noreply.github.com>
2022-12-05 11:40:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,26 +9,20 @@ import streamlit as st
 
 st.title("冷蔵庫の賞味期限")
 
-left_column,right_column = st.columns(2)
 food1 = st.text_input('食材は何ですか？')
-term1 = st.date_input(food1,'の食材の賞味期限はいつですか？',)
-left_column.write(term1)
+term1 = st.date_input('の食材の賞味期限はいつですか？',)
 
 food2 = st.text_input('食材は何ですか？')
-term2 = st.date_input(food2,'の賞味期限はいつですか？',)
-left_column.write(term2)
+term2 = st.date_input('の賞味期限はいつですか？',)
 
 food3 = st.text_input('食材は何ですか？')
-term3 = st.date_input(food3,'の賞味期限はいつですか？',)
-left_column.write(term3)
+term3 = st.date_input('の賞味期限はいつですか？',)
 
 food3 = st.text_input('食材は何ですか？')
-term3 = st.date_input(food3,'の賞味期限はいつですか？',)
-left_column.write(term3)
+term3 = st.date_input('の賞味期限はいつですか？',)
 
 food4 = st.text_input('食材は何ですか？')
-term4 = st.date_input(food4,'の賞味期限はいつですか？',)
-left_column.write(term4)
+term4 = st.date_input('の賞味期限はいつですか？',)
 
 import pandas as pd
 st.table(pd.DataFrame({
