80799e437eb72cfb900a95c0936e6caacafeeea7
21e1806a <119286019+21e1806a@users.noreply.github.com>
2022-12-05 12:23:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,25 +9,17 @@ import streamlit as st
 
 st.title("冷蔵庫の賞味期限")
 
-food1 = st.text_input('食材は何ですか？')
-term1 = st.date_input('賞味期限はいつですか？',)
+food1 = st.text_input('一つ目の食材は何ですか？')
+term1 = st.date_input('一つ目の賞味期限はいつですか？',)
 
-food2 = st.text_input('食材は何ですか？')
-term2 = st.date_input('賞味期限はいつですか？',)
+food2 = st.text_input('二つ目の食材は何ですか？')
+term2 = st.date_input('二つ目の賞味期限はいつですか？',)
 
-food3 = st.text_input('食材は何ですか？')
-term3 = st.date_input('賞味期限はいつですか？',)
-
-food3 = st.text_input('食材は何ですか？')
-term3 = st.date_input('賞味期限はいつですか？',)
-
-food4 = st.text_input('食材は何ですか？')
-term4 = st.date_input('賞味期限はいつですか？',)
+food3 = st.text_input('三つ目の食材は何ですか？')
+term3 = st.date_input('三つ目の賞味期限はいつですか？',)
 
 import pandas as pd
-df=pd.DataFrame({
-    '食材':pd.Series([food1, food2, food3, food4], index = [ 1, 2, 3, 4 ] ),
-    '賞味期限': pd.Series([term1, term2, term3, term4], index = [ 1, 2, 3, 4 ] )
+df=pd.DataFrame({'賞味期限': pd.Series([term1, term2, term3], index = [food1, food2, food3])
     }) 
 
 df.sort_values( by = '賞味期限', ascending = True, inplace = True )
