336181f12feb9ab4998150d0bd28b4f1f03545bc
21e1806a <119286019+21e1806a@users.noreply.github.com>
2022-12-05 15:12:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,10 @@ food3 = st.text_input('三つ目の食材は何ですか？')
 term3 = st.date_input('三つ目の賞味期限はいつですか？',)
 
 import pandas as pd
+list1=[food1,food2,food3]
+pd.DataFrame(data=list1)
+
+import pandas as pd
 df=pd.DataFrame({
     '食材': pd.Series([food1, food2, food3], index = [1, 2, 3]),
     '賞味期限': pd.Series([term1, term2, term3], index = [1, 2, 3])
@@ -26,3 +30,5 @@ df=pd.DataFrame({
 
 df.sort_values( by = '賞味期限', ascending = True, inplace = True )
 print(df)
+
+
