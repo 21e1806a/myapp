b5396c17184ba62a09b2012ef62e3abe0b1dbb4a
21e1806a <119286019+21e1806a@users.noreply.github.com>
2022-12-05 12:39:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,13 +13,16 @@ food1 = st.text_input('一つ目の食材は何ですか？')
 term1 = st.date_input('一つ目の賞味期限はいつですか？',)
 
 food2 = st.text_input('二つ目の食材は何ですか？')
-term2 = st.date_input('二つ目の賞味期限はいつですか？',)
+term2 = st.date_input("二つ目の賞味期限はいつですか？",)
 
 food3 = st.text_input('三つ目の食材は何ですか？')
 term3 = st.date_input('三つ目の賞味期限はいつですか？',)
 
 import pandas as pd
-df=pd.DataFrame({'賞味期限': pd.Series([term1, term2, term3], index = [food1, food2, food3])
+df=pd.DataFrame({
+    '食材': pd.Series([food1, food2, food3], index = [1, 2, 3]),
+    '賞味期限': pd.Series([term1, term2, term3], index = [1, 2, 3])
     }) 
 
 df.sort_values( by = '賞味期限', ascending = True, inplace = True )
+print(df)
