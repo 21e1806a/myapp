73b7a232a2005f16f454e6a245befb43c0ad944d
21e1806a <119286019+21e1806a@users.noreply.github.com>
2022-12-05 11:22:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,10 +12,11 @@ st.title("冷蔵庫の賞味期限")
 left_column,right_column = st.columns(2)
 food1 = st.date_input('1つ目の食材の賞味期限はいつですか？',)
 left_column.write(food1)
+
 import pandas as pd
 st.table(pd.DataFrame({
-    '食材': [food1, 2, 3, 4],
-    '賞味期限': [10, 20, 30, 40]}))
+    '食材':[1, 2, 3, 4],
+    '賞味期限': [food1, food2, food3, food4]}))
 
 
 
@@ -26,18 +27,18 @@ food1 = st.date_input('1つ目の食材の賞味期限はいつですか？',)
 left_column.write(food1)   
     
 @st.cache(allow_output_mutation=True)
-def cache_lst1():
-    lst1 = []
-    return lst1
-lst1 = cache_lst1()
+def cache_DateFrame():
+    pd.DateFrame = []
+    return pd.DateFrame
+DateFrame = cache_DateFrame()
 input = st.text_input('食材は何ですか？')
 if input:
-    lst1.append(input)    
+    pd.DateFrame.append(input)    
 
 if st.checkbox('delete1'):
-    delete = st.selectbox('食べたらリストから削除しましょう', options=lst1)
+    delete = st.selectbox('食べたらリストから削除しましょう', options=pd.DateFrame)
     if st.button('Delete1'):
-        lst1.remove(delete)
+        pd.DateFrame.remove(delete)
         st.success(f'Delete1 : {delete}')
         
-st.table(lst1)
+st.table(pd.DateFrame)
